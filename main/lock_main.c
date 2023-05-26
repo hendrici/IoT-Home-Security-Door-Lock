8393bd4aeca86261060ba23ee0b0385e48830a4a
Isaiah Hendrick <hendrici@mail.gvsu.edu>
2023-05-26 16:23:40 -0400

    rearrange code, LCD now working

--- main/lock_main.c
+++ main/lock_main.c
@@ -1,4 +1,6 @@
+/************************** INCLUDE STATEMENTS *******************************/
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
@@ -25,15 +27,16 @@
 #include "esp_log.h"
 #include "mqtt_client.h"
 
-#define LED_PIN 2
-#define SERVO_PIN 4
+/************************** PIN MACRO DEFINITIONS *******************************/
+#define LED_PIN                 GPIO_NUM_2
+#define SERVO_PIN               GPIO_NUM_4
 
-#define LCD_Enable GPIO_NUM_22
-#define LCD_RS GPIO_NUM_23
-#define LCD_DB4 GPIO_NUM_32
-#define LCD_DB5 GPIO_NUM_33
-#define LCD_DB6 GPIO_NUM_34
-#define LCD_DB7 GPIO_NUM_35
+#define LCD_Enable              GPIO_NUM_22
+#define LCD_RS                  GPIO_NUM_23
+#define LCD_DB4                 GPIO_NUM_32
+#define LCD_DB5                 GPIO_NUM_33
+#define LCD_DB6                 GPIO_NUM_25
+#define LCD_DB7                 GPIO_NUM_26
 
 #define LEDC_TIMER              LEDC_TIMER_0
 #define LEDC_MODE               LEDC_LOW_SPEED_MODE
@@ -45,17 +48,65 @@
 
 #define LEDC_FREQUENCY          (50)
 
-#define CONFIG_BROKER_URL "mqtt://test.mosquitto.org/"
+#define CONFIG_BROKER_URL       "mqtt://test.mosquitto.org/"
 
 static const char *TAG = "MQTT_EXAMPLE";
 
 esp_mqtt_client_handle_t mqtt_client;
 
+/************************** FUNCTION PROTOTYPES *******************************/
+void ledBlink(void *pvParams);
+static void logErrorIfNonzero(const char *message, int error_code);
+void printDeviceInfo(void);
 void lockBolt(void);
-
 void unlockBolt(void);
+void lockInit(void);
+void initLCD(void);
+void initSequenceLCD(void);
+void pulseEnable(void);
+void push_nibble(uint8_t var);
+void push_byte(uint8_t var);
+void commandWrite(uint8_t var);
+void dataWrite(uint8_t var);
+void writeEnterPinScreen(void);
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
+static void mqtt_app_start(void);
 
-void led_blink(void *pvParams) {
+/************************** FUNCTIONS *******************************/
+void app_main(void) {
+    printDeviceInfo();
+
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+
+    /* WIFI FUNCTIONALITY
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_ERROR_CHECK(example_connect());
+
+    mqtt_app_start();
+    
+    */
+
+    initLCD();
+    writeEnterPinScreen();
+    
+    // lockInit();
+    // xTaskCreate(&led_blink,"LED_BLINK",2048,NULL,5,NULL);
+}
+
+void ledBlink(void *pvParams) {
     esp_rom_gpio_pad_select_gpio(LED_PIN);
     gpio_set_direction (LED_PIN,GPIO_MODE_OUTPUT);
     while (1) { 
@@ -68,24 +119,48 @@ void led_blink(void *pvParams) {
     }
 }
 
-static void log_error_if_nonzero(const char *message, int error_code)
-{
+static void logErrorIfNonzero(const char *message, int error_code)   {
     if (error_code != 0) {
         ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
     }
 }
 
-void lockBolt(void){
+void printDeviceInfo(void)  {
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    uint32_t flash_size;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
+        return;
+    }
+
+    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+}
+
+void lockBolt(void) {
     ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_LOCKED);
     ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
 }
 
-void unlockBolt(void){
+void unlockBolt(void)   {
     ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_UNLOCKED);
     ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
 }
 
-void lockInit(void){
+void lockInit(void) {
     ledc_timer_config_t ledc_timer = {
         .speed_mode       = LEDC_MODE,
         .timer_num        = LEDC_TIMER,
@@ -108,13 +183,7 @@ void lockInit(void){
     ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
 
-/**
- * @brief The initLCD function configures the ESP32 pins: 
- * 22, 23, 32, 33, 34, and 35 as outputs and drives them low
- * 
- */
-void initLCD (void) {
-    
+void initLCD(void) {
     // Initialize LCD GPIOs as outputs
     gpio_set_direction(LCD_DB4, GPIO_MODE_OUTPUT); 
     gpio_set_direction(LCD_DB5, GPIO_MODE_OUTPUT);
@@ -129,20 +198,52 @@ void initLCD (void) {
     gpio_set_level(LCD_DB6, 0); 
     gpio_set_level(LCD_DB7, 0); 
     gpio_set_level(LCD_RS, 0); 
-    gpio_set_level(LCD_Enable, 0); 
+    gpio_set_level(LCD_Enable, 0);
+
+    initSequenceLCD();
 }
 
-/**
- * @brief The pulseEnable 
- * 
- */
+void initSequenceLCD(void) {
+    vTaskDelay(1/portTICK_PERIOD_MS);
+
+    // reset sequence
+    commandWrite(0x3); 
+    vTaskDelay(10/portTICK_PERIOD_MS);
+    commandWrite(0x3); 
+    vTaskDelay(1/portTICK_PERIOD_MS);
+    commandWrite(0x3); 
+    vTaskDelay(10/portTICK_PERIOD_MS);
+
+    // set to 4-bit mode
+    commandWrite(0x2);
+    vTaskDelay(1/portTICK_PERIOD_MS);
+    commandWrite(0x2);
+    vTaskDelay(1/portTICK_PERIOD_MS);
+
+    // // 2 lines, 5x7 format
+    // commandWrite(0x8);
+    // vTaskDelay(1/portTICK_PERIOD_MS);
+
+    // turns display and cursor ON, blinking
+    commandWrite(0xF);
+    vTaskDelay(1/portTICK_PERIOD_MS);
+
+    // clear display, move cursor to home
+    commandWrite(0x1);
+    vTaskDelay(1/portTICK_PERIOD_MS);
+
+    // increment cursor
+    commandWrite(0x6);
+    vTaskDelay(1/portTICK_PERIOD_MS);
+}
+
 void pulseEnable(void) {
 
     gpio_set_level(LCD_Enable, 0); 
-    vTaskDelay(10/portTICK_PERIOD_MS);
+    vTaskDelay(1/portTICK_PERIOD_MS);
 
     gpio_set_level(LCD_Enable, 1);
-    vTaskDelay(10/portTICK_RATE_MICROSECONDS);
+    vTaskDelay(1/portTICK_PERIOD_MS);
 
     gpio_set_level(LCD_Enable, 0); 
 }
@@ -151,67 +252,71 @@ void push_nibble(uint8_t var) {
 
     // Drive Pins Low
     gpio_set_level(LCD_DB7, 0); 
-    gpio_set_level(LCD_DB7, 0);
-    gpio_set_level(LCD_DB7, 0); 
-    gpio_set_level(LCD_DB7, 0);
+    gpio_set_level(LCD_DB6, 0);
+    gpio_set_level(LCD_DB5, 0); 
+    gpio_set_level(LCD_DB4, 0);
 
     // Set Respective Pin 
-    gpio_set_level(LCD_DB7, nibble >> 3); 
-    gpio_set_level(LCD_DB7, (nibble >> 2) & 1); 
-    gpio_set_level(LCD_DB7, (nibble >> 1) & 1); 
-    gpio_set_level(LCD_DB7, nibble & 1); 
+    gpio_set_level(LCD_DB7, var >> 3); 
+    gpio_set_level(LCD_DB6, (var >> 2) & 1); 
+    gpio_set_level(LCD_DB5, (var >> 1) & 1); 
+    gpio_set_level(LCD_DB4, var & 1); 
 
     pulseEnable(); 
 }
 
 void push_byte(uint8_t var) {
-    push_nibble(var >> 4)
-    push_nibble(var & 0x0F)
-    vTaskDelay(100/portTICK_PERIOD_MS);
+    push_nibble(var >> 4);
+    push_nibble(var & 0x0F);
+    vTaskDelay(1/portTICK_PERIOD_MS);
 }
 
-/**
- * @brief 
- * 
- * @param var 
- */
 void commandWrite(uint8_t var) {
     gpio_set_level(LCD_RS, 0); 
-    vTaskDelay(2000/portTICK_PERIOD_MS);
-
+    vTaskDelay(1/portTICK_PERIOD_MS);
     push_byte(var); 
 }
 
-/**
- * @brief 
- * 
- * @param var 
- */
 void dataWrite(uint8_t var) {
     gpio_set_level(LCD_RS, 1); 
-    vTaskDelay(2000/portTICK_PERIOD_MS);
-
+    vTaskDelay(1/portTICK_PERIOD_MS);
     push_byte(var); 
 }
 
-void initLCD {
-    vTaskDelay(100/portTICK_PERIOD_MS);
-
-    // reset 
-    commandWrite(3); 
-    vTaskDelay(100/portTICK_PERIOD_MS);
-
-    commandWrite(0x3); 
-    vTaskDelay(100/portTICK_PERIOD_MS);
-
-    commandWrite(0x3); 
-    vTaskDelay(2000/portTICK_PERIOD_MS);
-
-    commandWrite(2);
-    vTaskDelay(2/portTICK_PERIOD_MS);
+void changeScreenStateLCD(void) {
 
 }
 
+void writeUnlockScreen(bool isRemote)    {
+
+}
+
+void writeLockScreen(bool isRemote) {
+    if (isRemote)   {
+
+    } else  {
+
+    }
+}
+
+void writeEnterPinScreen(void)  {
+    uint8_t i;
+    char name[33] = "    Isaiah H.         EGR       ";         // message for line 1 & 3
+    char name2[33] = "   Triston G.         227       ";        // message for line 2 & 4
+
+    for(i = 0; i < 32; i++){        // prints message for line 1 & 3
+        dataWrite(name[i]);
+        vTaskDelay(10/portTICK_PERIOD_MS);
+    }
+
+    commandWrite(0xC0);             // move to line 2 (address 0x40)-> 0b1100 0000
+
+    for(i = 0; i < 32; i++){        // prints message for line 2 & 4
+        dataWrite(name2[i]);
+        vTaskDelay(10/portTICK_PERIOD_MS);
+    }
+
+}
 
 /*
  * @brief Event handler registered to receive MQTT events
@@ -223,8 +328,7 @@ void initLCD {
  * @param event_id The id for the received event.
  * @param event_data The data for the event, esp_mqtt_event_handle_t.
  */
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-{
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)   {
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
     esp_mqtt_event_handle_t event = event_data;
     
@@ -258,9 +362,9 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
         if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            logErrorIfNonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            logErrorIfNonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            logErrorIfNonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
             ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
 
         }
@@ -271,8 +375,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     }
 }
 
-static void mqtt_app_start(void)
-{
+static void mqtt_app_start(void)    {
     esp_mqtt_client_config_t mqtt_cfg = {
         .broker.address.uri = CONFIG_BROKER_URL,
     };
@@ -282,60 +385,4 @@ static void mqtt_app_start(void)
     esp_mqtt_client_start(client);
 
     esp_mqtt_client_publish(client,"brendan/testing/open","test",0,0,0);
-}
-
-void printDeviceInfo(void){
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
-    }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-}
-
-void app_main(void) 
-{
-    printDeviceInfo();
-
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
-    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
-    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
-    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
-
-    /* WIFI FUNCTIONALITY
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    ESP_ERROR_CHECK(example_connect());
-
-    mqtt_app_start();
-    
-    */
-    lockInit();
-    // xTaskCreate(&led_blink,"LED_BLINK",2048,NULL,5,NULL);
-    
 }
