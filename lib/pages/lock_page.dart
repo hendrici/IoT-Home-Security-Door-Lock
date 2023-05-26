d03c0dcf05183471367540020e62c47cd4d870d1
coffmanb <112509868+coffmanb@users.noreply.github.com>
2023-05-26 15:18:19 -0400

    Working communication with lock

--- lib/pages/lock_page.dart
+++ lib/pages/lock_page.dart
@@ -1,38 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:lock_app/providers/lock_state_provider.dart';
 import 'package:lock_app/providers/mqtt_provider.dart';
+
+const lockStatusTopic = "brendan/lockStatus/";
+const pinOutputTopic = "brendan/pinEntry/";
 
 class LockPage extends HookConsumerWidget {
   const LockPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var pinInput = "none";
     ref.read(mqttProvider).connect();
     ref.read(mqttProvider).subscribe(
-        "brendan/lockStatus/",
+        lockStatusTopic,
         ((data) => ref
             .read(lockStateProvider.notifier)
-            .setLockState(data.contains("true"))));
+            .setLockState(data.contains("unlocked"))));
     return AppBar(
       title: const Text("Lock page"),
       flexibleSpace: Padding(
         padding: const EdgeInsets.all(8),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            const SizedBox(
+              height: 200,
+            ),
             Consumer(
               builder: (context, ref, child) {
-                final lockState = ref.watch(lockStateProvider);
+                var lockState = ref.watch(lockStateProvider);
                 return Icon(
-                  lockState != null ? Icons.lock : Icons.lock_open,
+                  lockState as bool ? Icons.lock_open : Icons.lock,
                   size: 100,
                 );
               },
             ),
-            const Text("Enter a pin"),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 5, 32, 5),
+              child: TextFormField(
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  hintText: 'Enter a pin',
+                  labelText: 'Pin',
+                ),
+                onChanged: (String? value) {
+                  pinInput = value ?? "none";
+                },
+                validator: (String? value) {
+                  return (value != null) ? 'Do not use the @ char.' : null;
+                },
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () =>
+                    ref.read(mqttProvider).publish(pinOutputTopic, pinInput),
+                child: const Text("Send")),
           ],
         ),
       ),
--- lib/providers/mqtt_provider.dart
+++ lib/providers/mqtt_provider.dart
@@ -6,24 +6,26 @@ import 'package:mqtt_client/mqtt_server_client.dart';
 final mqttProvider = Provider((_) => MqttProvider());
 
 class MqttProvider {
-  late MqttServerClient _client;
+  final client = MqttServerClient('test.mosquitto.org', 'testingLock');
   bool clientConnected = false;
 
   Future<void> connect() async {
-    final client = MqttServerClient('test.mosquitto.org', 'testingLock');
     client.autoReconnect = true;
-    client.logging(on: true);
+    // client.logging(on: true);
     await client.connect();
-    _client = client;
     clientConnected = true;
   }
 
   Future<void> subscribe(String topic, void Function(String) onData) async {
-    while (clientConnected) {}
-    _client.subscribe(topic, MqttQos.atMostOnce);
-    _client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> messages) {
-      for (var message in messages) {
-        final data = message.payload.toString();
+    Future.delayed(const Duration(seconds: 3),
+        (() => {client.subscribe(topic, MqttQos.atMostOnce)}));
+
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> messages) {
+      for (MqttReceivedMessage message in messages) {
+        var payload = message.payload.payload.message;
+        var data = String.fromCharCodes(payload);
+        print(message.payload.toString());
+        print(message.payload);
         onData(data);
       }
     });
@@ -32,10 +34,10 @@ class MqttProvider {
   Future<void> publish(String topic, String data) async {
     final builder = MqttClientPayloadBuilder();
     builder.addString(data);
-    _client.publishMessage(topic, MqttQos.atMostOnce, builder.payload!);
+    client.publishMessage(topic, MqttQos.atMostOnce, builder.payload!);
   }
 
   void disconnect() {
-    _client.disconnect();
+    client.disconnect();
   }
 }
